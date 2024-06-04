e392163db4d4a972ad5c17cf276a4c467715384a
JuliaNery <julianerysinigalia@gmail.com>
2024-06-04 02:56:53 -0300

    initial

---
+++ src/main/java/com/global/all4ocean/All4oceanApplication.java
@@ -0,0 +1,13 @@
+package com.global.all4ocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class All4oceanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(All4oceanApplication.class, args);
+	}
+
+}
